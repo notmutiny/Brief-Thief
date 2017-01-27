1cdf8fe45de471cf10111ef5dbbf1afc71ed34b1
mutiny <mutenous@users.noreply.github.com>
2017-01-27 13:30:53 -0800

    1.1b
    savings still broke af but I made the code cleaner

--- BriefThief.lua
+++ BriefThief.lua
@@ -1,44 +1,39 @@
--- Global Table
-BriefThief = {}
+-- Global table
+BriefThief={}
 
--- Initialize
-function BriefThief.Initialize()
-	self.savedVariables = ZO_SavedVars:New("BriefThiefVars", self.version, nil, self.Default)
-    EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
-end
-	
 local self=BriefThief
 
--- String Vars
+-- Initialize
+function BriefThief:Initialize()
+    self.savedVariables = ZO_SavedVars:New("BriefThiefVars", self.version, nil, self.Default)
+    EVENT_MANAGER:UnregisterForEvent(self.name,EVENT_ADD_ON_LOADED)
+end
+
+-- Variables
+self.version=1.1
 self.color={"|cffa700","orange"}
 self.prevColor={}
-self.confirm=0
+self.pushChat=0
 
--- Convenience Functions  
+-- Convienence Functions
 local function confirm()
-    if self.confirm == 0 then return end
-    if self.push == 0 then return end
+    if self.pushChat==0 then return end
+    if self.pushColor==0 then return end
     self.color=self.prevColor
     d(self.color[1].."Color has updated successfully.")
-    self.confirm = 0
-    self.push= 0
+    self.pushChat=0
+    self.pushColor=0
 end
 
 local function broadcast()
+    if self.color[1]==self.prevColor[1] then return end
     d(self.color[1].."Brief Thief has been set to |r"..self.prevColor[1]..""..self.prevColor[2].."|r"..self.color[1].."!")
     d(self.prevColor[1].."Type /lootyes to confirm changes.|r")
-    confirm()
+    self.pushChat=0
 end
 
-local function save()
---	if self.savedVariables.color then
---    self.savedVariables.color = self.color
---    end
---    else return 0 end
-end
-
-local function TableLength(tab)
-    if not(tab)then return 0 end
+ local function TableLength(tab)
+    if not(tab) then return 0 end
     local Result=0
     for key,value in pairs(tab)do
         Result=Result+1
@@ -51,10 +46,19 @@ local function ShowAllItemInfo(item)
     end
 end
 
+-- Savings still broke af
+local function save()
+--	if self.savedVariables.color then
+--    self.savedVariables.color = self.color
+--    end
+--    else return 0 end
+end
+
 -- Addon Member Vars
 function self:GetInventory()
     return PLAYER_INVENTORY.inventories[INVENTORY_BACKPACK].slots
 end
+
 function self:Check()
     local StolenNumber,StolenValue,Inventory=0,0,self:GetInventory()
     for key,item in pairs(Inventory)do
@@ -64,15 +68,15 @@ function self:Check()
             StolenValue=StolenValue+StackValue
         end
     end
-    local Plural="s"
-    if(StolenNumber==1)then Plural="" end
-    d(self.color[1]..""..tostring(StolenNumber).." stolen item"..Plural.." worth "..tostring(StolenValue).." gold|r")
-end
-
+    local plural="s"
+    if(StolenNumber==1)then plural="" end
+    d(self.color[1]..""..tostring(StolenNumber).." stolen item"..plural.." worth "..tostring(StolenValue).." gold|r")
+end  
+  
 -- Game hooks
-SLASH_COMMANDS["/lootyes"]=function() self.confirm=1 confirm() end
-SLASH_COMMANDS["/lootr"]=function() self.push=1 self.prevColor={"|cff0000","red"} broadcast() save() end
-SLASH_COMMANDS["/looto"]=function() self.push=1 self.prevColor={"|cffa700","orange"} broadcast() save() end
-SLASH_COMMANDS["/looty"]=function() self.push=1 self.prevColor={"|cffff00","yellow"} broadcast() save() end
+SLASH_COMMANDS["/lootyes"]=function() self.pushChat=1 confirm() end
+SLASH_COMMANDS["/lootr"]=function() self.pushColor=1 self.prevColor={"|cff0000","red"} broadcast() end
+SLASH_COMMANDS["/looto"]=function() self.pushColor=1 self.prevColor={"|cffa700","orange"} broadcast() end
+SLASH_COMMANDS["/looty"]=function() self.pushColor=1 self.prevColor={"|cffff00","yellow"} broadcast() end
 SLASH_COMMANDS["/loot"]=function() self:Check() end
 EVENT_MANAGER:RegisterForEvent("BriefThief_ArrestCheck",EVENT_JUSTICE_BEING_ARRESTED,function() self:Check() end)
