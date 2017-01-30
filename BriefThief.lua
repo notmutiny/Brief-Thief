b6aa6a7569e2944b3f211000eb275301b599668b
mutiny <mutenous@users.noreply.github.com>
2017-01-29 20:40:36 -0800

    updating to current 1.7 release
    spaghetti code grows up so fast :')

--- BriefThief.lua
+++ BriefThief.lua
@@ -1,6 +1,6 @@
 -- Global table
 BriefThief={
-	version=1.5.2,
+	version=1.7,
 	colors={ 				-- this is what i'd call data-driven
 		red="|cff0000", 	-- all you gotta do to add new colors is just add entries to the table
 		green="|c00ff00", 	-- no if elseif polling, no changing code
@@ -21,10 +21,12 @@ BriefThief={
 	},
 	curColor="",
 	prevColor="",
-	guards=true,
-	fences=true,
+	showGuard=true,
+	showFence=true,
 	defaultPersistentSettings={
-		color="orange"
+		color="orange",
+		guard=true,
+		fence=true
 	},
 	persistentSettings={}
 }
@@ -49,8 +51,8 @@ end
 function BriefThief:Initialize()
 	self.persistentSettings=ZO_SavedVars:NewAccountWide("BriefThiefVars",self.version,nil,self.defaultPersistentSettings) -- load in the persistent settings
 	self.curColor=self.persistentSettings.color -- set our current color to whatever came from the settings file
-	self.guards=self.persistentSettings.guards
-	self.fences=self.persistentSettings.fences
+	self.showGuard=self.persistentSettings.guard -- sets briefthief to guard settings
+	self.showFence=self.persistentSettings.fence -- sets briefthief to fence settings
 	EVENT_MANAGER:UnregisterForEvent("BriefThief_OnLoaded",EVENT_ADD_ON_LOADED) -- not really sure if we have to do this
 end
 
@@ -59,7 +61,7 @@ function BriefThief:ChangeColor(color)
 	if not(self.colors[newColor])then return end -- if the word they typed isn't a color we support then fuck em
 	if(newColor==self.curColor)then return end -- if we're already that color then fuck em
 	local OldHex,NewHex=self.colors[self.curColor],self.colors[newColor]
-	d(OldHex.."Brief Thief has changed to "..NewHex..newColor..OldHex.."!|r")
+	d(OldHex.."Brief Thief has changed to "..NewHex..newColor)
 	self.prevColor=self.curColor
 	self.curColor=newColor
 	self.persistentSettings.color=self.curColor -- save the setting in ESO settings file
@@ -87,14 +89,14 @@ function BriefThief:Check()
     self:Chat(tostring(StolenNumber).." stolen item"..plural.." worth "..tostring(StolenValue).." gold")
 end
 
-function BriefThief:HandleEvent(arg)
+function BriefThief:HandleEvent(arg) -- this sorts where the /loot (event) arguement should go
 	if (arg=="guard") then
-		if (BriefThief.guards) then
+		if (BriefThief.showGuard) then
 			BriefThief:ToggleEvent("not",arg)			
 		else
 			BriefThief:ToggleEvent("",arg) end
 	else if (arg=="fence") then
-		if (BriefThief.fences) then
+		if (BriefThief.showFence) then
 			BriefThief:ToggleEvent("not",arg)
 		else
 			BriefThief:ToggleEvent("",arg) end
@@ -102,7 +104,7 @@ function BriefThief:HandleEvent(arg)
 	end
 end
 
-function BriefThief:ToggleEvent(string,arg)
+function BriefThief:ToggleEvent(string,arg) -- this is the main function after the previous sorted it
 	if(string=="not") then
 		d(BriefThief.colors[BriefThief.curColor].."Brief Thief will "..string.." show when talking to "..arg.."s|r")
 	elseif(string=="") then
@@ -110,25 +112,25 @@ function BriefThief:ToggleEvent(string,arg)
 	else
 		return end
 	if(arg=="guard") then
-		BriefThief.guards=not BriefThief.guards
-		BriefThief.persistentSettings.guards=BriefThief.guards
+		BriefThief.showGuard=not BriefThief.showGuard
+		BriefThief.persistentSettings.guard=BriefThief.showGuard
 	else
-		BriefThief.fences=not BriefThief.fences	
-		BriefThief.persistentSettings.fences=BriefThief.fences	
+		BriefThief.showFence=not BriefThief.showFence	
+		BriefThief.persistentSettings.fence=BriefThief.showFence
 	end
 end
 
-function BriefThief:GuardEventFix()
-	if (BriefThief.guards) then BriefThief:Check()
-    else return end
+function BriefThief:EventFix(who) -- this tells our registered events to not fire if user specified
+	if (who=="guard") then
+		if(BriefThief.showGuard) then BriefThief:Check() end
+	elseif (who=="fence") then
+		if(BriefThief.showFence) then BriefThief:Check() end
+	else return end
 end		
 
-function BriefThief:FenceEventFix()
-	if (BriefThief.fences) then BriefThief:Check()
-    else return end
-end	
-
 -- Game hooks
+SLASH_COMMANDS["/halp"]=function() d(BriefThief.showFence) end
+
 SLASH_COMMANDS["/loot"]=function(arg) 
     if (arg=="guard" or arg=="fence") then BriefThief:HandleEvent(arg)
 	else if ((arg) and (arg~="")) then
@@ -138,6 +140,6 @@ SLASH_COMMANDS["/loot"]=function(arg)
 end
 end
 
-EVENT_MANAGER:RegisterForEvent("BriefThief_OpenFence",EVENT_OPEN_FENCE,function() BriefThief:FenceEventFix() end)
-EVENT_MANAGER:RegisterForEvent("BriefThief_ArrestCheck",EVENT_JUSTICE_BEING_ARRESTED,function() BriefThief:GuardEventFix() end)
+EVENT_MANAGER:RegisterForEvent("BriefThief_OpenFence",EVENT_OPEN_FENCE,function() BriefThief:EventFix("fence") end)
+EVENT_MANAGER:RegisterForEvent("BriefThief_ArrestCheck",EVENT_JUSTICE_BEING_ARRESTED,function() BriefThief:EventFix("guard") end)
 EVENT_MANAGER:RegisterForEvent("BriefThief_OnLoaded",EVENT_ADD_ON_LOADED,function() BriefThief:Initialize() end)
